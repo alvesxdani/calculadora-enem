14c9b1cf5aa05439da32fb6d40b06c94bd489ad3
alvesxdani <dani.alvesm22@gmail.com>
2023-09-05 15:30:42 -0300

    Add more weight to the nav menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({ ano }) => {
     return (
         <header className='w-[100%] bg-blue text-white p-5 mb-3 flex justify-center'>
-            <div className='flex justify-between w-[70%]'>
+            <div className='flex justify-between w-[80%]'>
                 <h1 className='font-bold font-montserrat text-xl'>Calculadora Enem {ano}</h1>
                 <nav>
                     <ul>
