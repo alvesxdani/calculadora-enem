5cce876510829d527dd72cd39c51955de589ebfb
alvesxdani <dani.alvesm22@gmail.com>
2023-09-25 17:56:26 -0300

    Start to implement the courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,105 @@
-import { useEffect, useState } from "react"
-import './index.css'
+import { useEffect, useState } from "react";
+import "./index.css";
 import Header from "./components/Header/Header";
 import fetchData from "./api/api";
 import Tables from "./components/Tables/Tables";
 
 function App() {
-
-  const [data, setData] = useState([])
-  const [turma, setTurma] = useState(null)
-  const [simulado1, setSimulado1] = useState({})
-  const [simulado2, setSimulado2] = useState({})
-  const [simulado3, setSimulado3] = useState({})
-  const [simulado4, setSimulado4] = useState({})
-  const [simulado5, setSimulado5] = useState({})
-  const [simulado6, setSimulado6] = useState({})
-  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [turma, setTurma] = useState(null);
+  const [simulado1, setSimulado1] = useState({});
+  const [simulado2, setSimulado2] = useState({});
+  const [simulado3, setSimulado3] = useState({});
+  const [simulado4, setSimulado4] = useState({});
+  const [simulado5, setSimulado5] = useState({});
+  const [simulado6, setSimulado6] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
 
     fetchData()
-      .then(response => {
-        setData(response)
+      .then((response) => {
+        setData(response);
       })
-      .catch(e => console.log(e))
+      .catch((e) => console.log(e))
       .finally(() => {
-        setLoading(false)
-      })
+        setLoading(false);
+      });
   }, []);
 
-
   function handleInfoAluno(value) {
-    data[0].filter(item => {
-      item.nome === value ? setSimulado1({
-        'turma': setTurma(item.turma),
-        'ling': item.ling.toFixed(1),
-        'mat': item.mat.toFixed(1),
-        'ch': item.ch.toFixed(1),
-        'cn': item.cn.toFixed(1)
-      }) : null
-    })
+    data[0].filter((item) => {
+      item.nome === value
+        ? setSimulado1({
+            turma: setTurma(item.turma),
+            ling: item.ling.toFixed(1),
+            mat: item.mat.toFixed(1),
+            ch: item.ch.toFixed(1),
+            cn: item.cn.toFixed(1),
+          })
+        : null;
+    });
 
-    data[1].filter(item => {
-      item.nome === value ? setSimulado2({
-        'turma': setTurma(item.turma),
-        'ling': item.ling.toFixed(1),
-        'mat': item.mat.toFixed(1),
-        'ch': item.ch.toFixed(1),
-        'cn': item.cn.toFixed(1)
-      }) : null
-    })
+    data[1].filter((item) => {
+      item.nome === value
+        ? setSimulado2({
+            turma: setTurma(item.turma),
+            ling: item.ling.toFixed(1),
+            mat: item.mat.toFixed(1),
+            ch: item.ch.toFixed(1),
+            cn: item.cn.toFixed(1),
+          })
+        : null;
+    });
 
-    data[2].filter(item => {
-      item.nome === value ? setSimulado3({
-        'turma': setTurma(item.turma),
-        'ling': item.ling.toFixed(1),
-        'mat': item.mat.toFixed(1),
-        'ch': item.ch.toFixed(1),
-        'cn': item.cn.toFixed(1)
-      }) : null
-    })
+    data[2].filter((item) => {
+      item.nome === value
+        ? setSimulado3({
+            turma: setTurma(item.turma),
+            ling: item.ling.toFixed(1),
+            mat: item.mat.toFixed(1),
+            ch: item.ch.toFixed(1),
+            cn: item.cn.toFixed(1),
+          })
+        : null;
+    });
 
-    data[3].filter(item => {
-      item.nome === value ? setSimulado4({
-        'turma': setTurma(item.turma),
-        'ling': item.ling.toFixed(1),
-        'mat': item.mat.toFixed(1),
-        'ch': item.ch.toFixed(1),
-        'cn': item.cn.toFixed(1)
-      }) : null
-    })
+    data[3].filter((item) => {
+      item.nome === value
+        ? setSimulado4({
+            turma: setTurma(item.turma),
+            ling: item.ling.toFixed(1),
+            mat: item.mat.toFixed(1),
+            ch: item.ch.toFixed(1),
+            cn: item.cn.toFixed(1),
+          })
+        : null;
+    });
 
-    data[4].filter(item => {
-      item.nome === value ? setSimulado5({
-        'turma': setTurma(item.turma),
-        'ling': item.ling.toFixed(1),
-        'mat': item.mat.toFixed(1),
-        'ch': item.ch.toFixed(1),
-        'cn': item.cn.toFixed(1)
-      }) : null
-    })
+    data[4].filter((item) => {
+      item.nome === value
+        ? setSimulado5({
+            turma: setTurma(item.turma),
+            ling: item.ling.toFixed(1),
+            mat: item.mat.toFixed(1),
+            ch: item.ch.toFixed(1),
+            cn: item.cn.toFixed(1),
+          })
+        : null;
+    });
 
-    data[5].filter(item => {
-      item.nome === value ? setSimulado6({
-        'turma': setTurma(item.turma),
-        'ling': item.ling.toFixed(1),
-        'mat': item.mat.toFixed(1),
-        'ch': item.ch.toFixed(1),
-        'cn': item.cn.toFixed(1)
-      }) : null
-    })
+    data[5].filter((item) => {
+      item.nome === value
+        ? setSimulado6({
+            turma: setTurma(item.turma),
+            ling: item.ling.toFixed(1),
+            mat: item.mat.toFixed(1),
+            ch: item.ch.toFixed(1),
+            cn: item.cn.toFixed(1),
+          })
+        : null;
+    });
   }
 
   return (
@@ -97,44 +107,62 @@ function App() {
       <Header ano={2023} />
 
       <main className="p-2">
-
-        <div className="container max-w-[600px] my-0 mx-auto border border-[#eee] rounded-sm p-3 text-dark-grey">
-          <div className="flex flex-col sm:flex-row gap-2 sm:gap-0 justify-between mb-4 align-middle">
-            <label htmlFor="name" className="font-bold">Nome do(a) aluno(a): </label>
+        <div
+          className="container flex flex-wrap max-w-[1064px] my-0 mx-auto border border-[#eee] rounded-sm p-3 text-dark-grey"
+        >
+          <div className="flex flex-col gap-3 justify-around mb-4 align-middle w-[100%]">
             
+            <div className="flex gap-3">
+            <label htmlFor="name" className="font-bold">
+              Nome do(a) aluno(a):{" "}
+            </label>
+
             {loading && <span>Carregando...</span>}
 
-            {!loading && <select
-              id="name"
-              className="border rounded-sm p-1"
-              onChange={({ target }) => handleInfoAluno(target.value)}
-            >
-              {data[0] && data[0].map((item, index) => {
-                return (
-                  <option id="name" key={index} value={item.nome}>{item.nome}</option>
-                )
-              })}
+            {!loading && (
+              <select
+                id="name"
+                className="border rounded-sm p-1 grow"
+                onChange={({ target }) => handleInfoAluno(target.value)}
+              >
+                {data[0] &&
+                  data[0].map((item, index) => {
+                    return (
+                      <option id="name" key={index} value={item.nome}>
+                        {item.nome}
+                      </option>
+                    );
+                  })}
+              </select>
+            )}
+            </div>
 
-            </select>
-            }
+            <div className="">
+              <span className="font-bold">Turma</span>: {turma}
+            </div>
+
+            <div>
+            <span className="font-bold">Curso</span>:
+            </div>
           </div>
 
-          <div className="mb-4">
-            <span className="font-bold">Turma</span>: {turma}
+          <div className="w-[50%] mr-1">
+            <Tables {...simulado1} num={1} />
+            <Tables {...simulado2} num={2} />
+            <Tables {...simulado3} num={3} />
+            <Tables {...simulado4} num={4} />
+            <Tables {...simulado5} num={5} />
+            <Tables {...simulado6} num={6} />
           </div>
 
-          <Tables {...simulado1} num={1} />
-          <Tables {...simulado2} num={2} />
-          <Tables {...simulado3} num={3} />
-          <Tables {...simulado4} num={4} />
-          <Tables {...simulado5} num={5} />
-          <Tables {...simulado6} num={6} />
+          <div className="w-[40%]">
+            Teste
+          </div>
+
         </div>
       </main>
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types'
 const Header = ({ ano }) => {
     return (
         <header className='w-[100%] bg-blue text-white p-5 mb-3 flex justify-center'>
-            <div className='flex justify-between w-[80%]'>
-                <h1 className='font-bold font-montserrat text-xl'>Calculadora Enem {ano}</h1>
-                <nav>
+            <div className='flex justify-center w-[80%]'>
+                
+                <a href="/">
+                    <h1 className='font-bold font-montserrat text-xl'>Calculadora Enem {ano}</h1>
+                </a>
+                {/* <nav>
                     <ul>
                         <li className='text-white'>
                             <a href="/">Início</a>
                         </li>
                     </ul>
-                </nav>
+                </nav> */}
             </div>
         </header>
     )
